9d804c54947887cff86fd0fc1a4bec2bacc9cb33
Andy Maloney <asmaloney@gmail.com>
2015-07-21 08:31:48 -0400

    Make OpenMP optional

--- Src/SurfaceTrimmer.cpp
+++ Src/SurfaceTrimmer.cpp
@@ -29,7 +29,9 @@ DAMAGE.
 #include <stdio.h>
 #include <stdlib.h>
 #include <float.h>
+#ifdef _OPENMP
 #include <omp.h>
+#endif
 #include <algorithm>
 #include "CmdLineParser.h"
 #include "Geometry.h"
